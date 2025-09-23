086ed943f974803c9bcbfa1143534f19634dad18
Uma-rajesh <39452397+Uma-rajesh@users.noreply.github.com>
2025-09-23 13:22:54 -0400

    Add files via upload

---
+++ Day3/01-looping.js
@@ -0,0 +1,39 @@
+// //For Loop - to print odd numbers
+
+// function numbers() {
+//     for (let i = 1; i <= 25; i++) {
+//          if (i % 2!==0)
+//        {
+//             console.log("odd numbers is:" ,i);
+//         }
+//     }
+// }
+// numbers();
+
+
+//while loop - print particular range
+// let count = 10
+// while (count) {
+//     console.log(count);
+//     count --
+// }
+
+//while loop 
+
+let count = 10;
+while (count>0) {
+    console.log("The value of count is: " +count);
+    count--;
+}
+
+let continueProgramming  = true;
+while(continueProgramming) {
+    console.log("Keep coding");   
+}
+
+let num = 6;
+do {
+    console.log("Number is: " +num);  
+    num++;
+} while (num<=5)
+
---
+++ Day3/HomeAssignment.js
@@ -0,0 +1,34 @@
+// const browserName = 'chrome';
+
+// function getBrowser() {
+
+//     if (browserName == 'chrome') {
+
+//         //let browserName ='firefox'
+//         console.log("Inside if block : "+ browserName);
+        
+        
+//     }
+//     var browserName ='Edge'
+//     console.log("Inside if block : "+ browserName);
+    
+// }
+// getBrowser()
+// console.log("Inside function : "+ browserName);
+
+
+const browserName = "chrome";
+
+function getBrowser() {
+
+    if (browserName === 'chrome') {
+
+       var browserName="firefox"
+        console.log("Inside if block : "+ browserName);          
+     } 
+     console.log("Outside block : "+ browserName);
+}
+
+getBrowser()
+
+
